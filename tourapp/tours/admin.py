dbbf0764dcc2dabad50c6cdfa32707ca7f2fd664
nnhatsang <nhatsang57@gmail.com>
2023-04-19 16:39:29 +0700

    update

--- tourapp/tours/admin.py
+++ tourapp/tours/admin.py
@@ -40,14 +40,12 @@ class TourForm(forms.ModelForm):
 
 
 class TourAdmin(admin.ModelAdmin):
-    model = Tour
-    exclude = ('tag',)
-    list_display = ('pk', 'name', 'attraction')
+    list_display = ('pk', 'name', 'attraction', 'departure_date', 'end_date', 'avatar')
     list_display_links = ('name',)
     search_fields = ('name',)
     form = TourForm
 
-    def view_image(self, new):
+    def avatar(self, new):
         if (new.image):
             return mark_safe(
                 '<img src="/{url}" width="120" />'.format(url=new.image.name)
@@ -55,25 +53,19 @@ class TourAdmin(admin.ModelAdmin):
 
 
 class ImageTourAdmin(admin.ModelAdmin):
-    model = ImageTour
-    readonly_fields = ('view_image',)
-
+    list_display = ('pk', 'view_image', 'tour', 'active')
+    search_fields = ('descriptions',)
     def view_image(self, obj):
         if (obj.image):
             return mark_safe(
                 '<img src="/{url}" width="120" />'.format(url=obj.image.name)
             )
 
-    fieldsets = (
-        ('Image of tour', {
-            'fields': ('tour', 'active', 'image_view', 'image')
-        }),
-    )
+
 
 class BookingTourAdmin(admin.ModelAdmin):
     list_display = ('pk', 'booking_information', 'created_date', 'updated_date', 'active')
     list_display_links = ('pk', 'booking_information', 'created_date', 'updated_date', 'active')
-
     def booking_information(self, obj):
         return obj.__str__()
 
@@ -81,7 +73,6 @@ class BookingTourAdmin(admin.ModelAdmin):
 class MyUserAdmin(UserAdmin):
     model = User
     search_fields = ('username', 'first_name', 'last_name')
-    exclude = ('group',)
     list_display = ('pk', 'username',)
     list_display_links = ('username',)
     list_filter = ('is_staff', 'is_superuser', 'is_customer')
@@ -93,12 +84,12 @@ class MyUserAdmin(UserAdmin):
                 '<img src="/{url}" width="120" />'.format(url=user.avatar.name)
             )
 
-    # add_fieldsets = (
-    #     (None, {
-    #         'classes': ('wide',),
-    #         'fields': ('avatar', 'username', 'password1', 'password2', 'email')}
-    #      ),
-    # )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('avatar', 'username', 'password1', 'password2', 'email')}
+         ),
+    )
 
     form = UserChangeForm
     add_form = UserCreationForm
@@ -125,4 +116,4 @@ admin.site.register(Tag)
 admin.site.register(Bill)
 admin.site.register(Like)
 admin.site.register(BookTour, BookingTourAdmin)
-admin.site.register(ImageTour)
+admin.site.register(ImageTour, ImageTourAdmin)
--- tourapp/tours/models.py
+++ tourapp/tours/models.py
@@ -12,6 +12,12 @@ class User(AbstractUser):
     def __str__(self):
         return self.username
 
+    # def save(self, *args, **kwargs):
+    #     if self.image:
+    #         super(AbstractUser, self).save(*args, **kwargs)
+    #         self.image.name = "static/{avt_name}".format(avt_name=self.avatar.name)
+    #         super(AbstractUser, self).save(*args, **kwargs)
+
 
 class BaseModel(models.Model):
     created_date = models.DateTimeField(auto_now_add=True)
@@ -41,19 +47,32 @@ class Tour(BaseModel):
     customers = models.ManyToManyField('User', through='BookTour', related_name='tours')
     tag = models.ManyToManyField('Tag', related_name='tours')
     description = RichTextField(null=True)
+    image = models.ImageField(upload_to='images/tours/%Y/%m/', null=True)
 
     def __str__(self):
         return self.name
+
+    # def save(self, *args, **kwargs):
+    #     if self.image:
+    #         super(BaseModel, self).save(*args, **kwargs)
+    #         self.image.name = "static/{img_name}".format(img_name=self.image.name)
+    #         super(BaseModel, self).save(*args, **kwargs)
 
 
 class ImageTour(BaseModel):
     image = models.ImageField(upload_to='images/tours/%Y/%m/', null=True)
     tour = models.ForeignKey('Tour', on_delete=models.SET_NULL, null=True)
-
+    descriptions = models.CharField(max_length = 255,null = True)
     class Meta:
         verbose_name = 'Image of tour'
+
     def __str__(self):
         return self.tour
+    def save(self, *args, **kwargs):
+        if self.image:
+            super(BaseModel, self).save(*args, **kwargs)
+            self.image.name = "static/{img_name}".format(img_name=self.image.name)
+            super(BaseModel, self).save(*args, **kwargs)
 
 
 class Attraction(BaseModel):
@@ -70,12 +89,10 @@ class BookTour(BaseModel):
     tour = models.ForeignKey('Tour', on_delete=models.CASCADE)
     num_of_adults = models.IntegerField(default=0)
     num_of_children = models.IntegerField(default=0)
-
     def __str__(self):
-        return " User: \"{0}\" Booking tour : \"{1}\" ".format(self.user.__str__(),self.tour.__str__())
+        return " User: \"{0}\" Booking tour : \"{1}\" ".format(self.user.__str__(), self.tour.__str__())
 
-    class Meta:
-        unique_together = ('user', 'tour')
+
 
 
 # user like cmt rating
@@ -85,35 +102,35 @@ class Comment(BaseModel):
     content = models.CharField(max_length=255, blank=True)
 
     def __str__(self):
-        return self.content
+        return "User: \"{0}\" --- \"{1}\" content: \"{2}\" ".format(self.user.__str__(), self.tour.__str__(),self.content.__str__())
+
+
 
 
 class Like(BaseModel):
     user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
     state = models.BooleanField(default=False)
     tour = models.ForeignKey('Tour', on_delete=models.CASCADE, related_name='likes', null=True)
-
-    class Meta:
-        unique_together = ('user', 'tour')
+    def __str__(self):
+        return " \"{0}\" --- Like (\"{1}\") Tour \"{2}\"".format(self.user.__str__(), self.state.__str__(),self.tour.__str__())
+    # class Meta:
+    #     unique_together = ('user', 'tour')
 
 
 class Rate(BaseModel):
     user = models.ForeignKey('User', on_delete=models.SET_NULL, null=True)
     star_rate = models.IntegerField(default=5)
     tour = models.ForeignKey('Tour', on_delete=models.CASCADE, related_name='rate', null=True)
-
     def __str__(self):
-        return " \"{0}\" --- \"{1}\" ".format(self.user.__str__(), self.tour.__str__())
-
-    class Meta:
-        unique_together = ('user', 'tour')
+        return " User \"{0}\" Rating tour: \"{1}\" : \"{2}\" *".format(self.user.__str__(), self.tour.__str__(),self.star_rate.__str__())
+    # class Meta:
+    #     unique_together = ('user', 'tour')
 
 
 # bill
 class Bill(BaseModel):
     book_tour = models.OneToOneField('BookTour', on_delete=models.CASCADE, primary_key=True)
     payment_state = models.BooleanField(default=False)
-
     total_price = models.FloatField(default=0)
 
     def __str__(self):
