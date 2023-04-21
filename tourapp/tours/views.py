e522777846ae07631a531d2f0fcb2274e833dafe
nnhatsang <nhatsang57@gmail.com>
2023-04-21 22:41:52 +0700

    view url setting

--- tourapp/tours/views.py
+++ tourapp/tours/views.py
@@ -6,25 +6,30 @@ from rest_framework.views import Response
 from .paginators import *
 
 
-class AttractionViewset(viewsets.ModelViewSet, generics.ListAPIView, generics.RetrieveAPIView):
+class AttractionViewSet(viewsets.ViewSet, generics.ListAPIView, generics.RetrieveAPIView):
     queryset = Attraction.objects.filter(active=True)
     serializer_class = AttractionSerializer
     pagination_class = AttractionPaginator
 
     def get_queryset(self):
-        q = self.queryset
+        query = self.queryset
         kw = self.request.query_params.get('kw')
         if kw:
-            q = q.filter(location__icontains=kw)
-        return q
+            query = query.filter(location__icontains=kw)
+        return query
 
     @action(methods=['get'], detail=True, url_path='tours')
     def tour(self, request, pk):
-        t=Attraction.objects.get
+        tours = Attraction.objects.get(pk=pk).tours
+        # tours=self.get_object()
+        kw = self.request.query_params('kw')
+        if kw is not None:
+            tours = tours.filter(name__icontains = kw)
+        return Response(AttractionSerializer(tours, many=True).data,status=status.HTTP_200_OK)
 
 
-class CommentViewset(viewsets.ModelViewSet, generics.ListAPIView, generics.RetrieveAPIView):
-    queryset = Comment.objects.all()
+class TourViewSet(viewsets.ViewSet, generics.UpdateAPIView, generics.DestroyAPIView):
+    queryset = Tour.objects.all()
     serializer_class = CommentSerializer
 
 # Create your views here.
